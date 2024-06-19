0f66bf48d057c4fd5389b00ab6acb89c849b4289
mykel68 <olowookeremicheal9568@gmail.com>
2024-06-19 19:31:00 +0100

    store item endpoint ready

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
+const storeItemRoutes = require("./routes/storeItemRoutes");
 const Sequelize = require("sequelize");
 const PORT = process.env.PORT || 5000;
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
@@ -28,6 +29,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/store-items", storeItemRoutes);
 
 const swaggerOptions = {
   swaggerDefinition: {
